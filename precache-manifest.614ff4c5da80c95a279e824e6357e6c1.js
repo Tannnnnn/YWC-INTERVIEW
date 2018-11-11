914b547d6c6e157ad83ef46014fd08d791290f81
Tannnnnn <tan_sweetmulet@hotmail.com>
2018-11-11 07:03:38 +0700

    Updates

---
+++ precache-manifest.614ff4c5da80c95a279e824e6357e6c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1053cf28fbc2e6d6c549",
+    "url": "/YWC-INTERVIEW/static/js/runtime~main.1053cf28.js"
+  },
+  {
+    "revision": "9451373cb7769423486f",
+    "url": "/YWC-INTERVIEW/static/js/main.9451373c.chunk.js"
+  },
+  {
+    "revision": "ed18152390015d292730",
+    "url": "/YWC-INTERVIEW/static/js/1.ed181523.chunk.js"
+  },
+  {
+    "revision": "9451373cb7769423486f",
+    "url": "/YWC-INTERVIEW/static/css/main.4a928b4c.chunk.css"
+  },
+  {
+    "revision": "ed18152390015d292730",
+    "url": "/YWC-INTERVIEW/static/css/1.16cd1889.chunk.css"
+  },
+  {
+    "revision": "385458d5f31c710a849aae16f263e94e",
+    "url": "/YWC-INTERVIEW/index.html"
+  }
+];
